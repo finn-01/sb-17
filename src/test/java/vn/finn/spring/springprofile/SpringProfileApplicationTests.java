baa9d2f614ff82e9ee5add15bf5f2ce474a838d2
finn-01 <nguyensam576@gmail.com>
2022-02-20 01:50:23 +0700

    create proj

---
+++ src/test/java/vn/finn/spring/springprofile/SpringProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.springprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
